efb3251afe0e77ddc2020b2a8b44107b2c33f211
Nick  Murimi <126444800+NickMurimi@users.noreply.github.com>
2023-06-09 21:43:15 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,4 @@
+myName = input("what's your name?:")
+myfavouritefood = input("what's your favourite food?:")
+myFavouriteartist = input("who's your favourite artist? ")
+myhome = input("where do you live? ")
